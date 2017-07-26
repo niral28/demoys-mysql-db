00df0d16a30e9cd3628911af7a1964bedc7796f7
Niral Shah <niralshah928@gmail.com>
2017-07-26 16:29:18 -0700

    fixed index.php

--- web/index.php
+++ web/index.php
@@ -1,17 +1,19 @@
 <?php
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$server = $url["host"];
+$username=$url["user"];
+$password=$url["pass"];
+$db=substr($url["path"],1);
 
-$db_name="diet";
-$mysql_user="root";
-$server_name="localhost";
-$connection=mysqli_connect($server_name,$mysql_user,"",$db_name);
-if(!$connection){
-  echo "Connection not successful";
+$connection=mysqli_connect($server,$username,$password,$db);
+if($connection){
+  echo "Connection successful";
   $httpStatusCode = 200;
   $httpStatusMsg  = 'OK';
   header('Status: '.$httpStatusCode.' '.$httpStatusMsg);
 }
 else{
-   echo "connection successful\n";
+   echo "connection not successful\n";
    $httpStatusCode = 304;
    $httpStatusMsg  = 'Not Modified';
    header('Status: '.$httpStatusCode.' '.$httpStatusMsg);
