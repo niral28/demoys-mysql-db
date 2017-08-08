64afe5d0c80c9f26ab9e8e31d9491bf5f4f050bb
Niral Shah <niralshah928@gmail.com>
2017-08-07 23:55:39 -0700

    fixed mistake in data.php

--- web/data.php
+++ web/data.php
@@ -8,7 +8,7 @@ $sql_query="SELECT * from user_data where username like '$u_username'";
 
 $encode = array();
 
-while($row = mysqli_fetch_assoc($result)) {
+while($row = mysqli_fetch_assoc($sql_query)) {
    $encode[] = $row;
 }
 
