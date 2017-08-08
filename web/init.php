de72eaf7022a26f4b0aa582d37cf35d53cbc36d3
Niral Shah <niralshah928@gmail.com>
2017-08-08 00:00:00 -0700

    testing changes to HTTP get request

--- web/init.php
+++ web/init.php
@@ -7,13 +7,13 @@ $db=substr($url["path"],1);
 
 $connection=mysqli_connect($server,$username,$password,$db);
 if($connection){
-  echo "Connection successful";
+  //echo "Connection successful\n";
   $httpStatusCode = 200;
   $httpStatusMsg  = 'OK';
   header('Status: '.$httpStatusCode.' '.$httpStatusMsg);
 }
 else{
-   echo "connection not successful\n";
+   //echo "connection not successful\n";
    $httpStatusCode = 304;
    $httpStatusMsg  = 'Not Modified';
    header('Status: '.$httpStatusCode.' '.$httpStatusMsg);
