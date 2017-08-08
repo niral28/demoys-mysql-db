457553cf81f23ae8744f0fc669d521872297e0c8
Niral Shah <niralshah928@gmail.com>
2017-08-07 23:33:16 -0700

    added HTTP GET calls

---
+++ web/data.php
@@ -0,0 +1,30 @@
+<?php
+
+require "init.php";
+$u_username  = $_GET["username"];
+
+
+$sql_query="SELECT name, lifestyle, current_weight, goal_weight from user_data where username like '$u_username'";
+
+$encode = array();
+
+while($row = mysqli_fetch_assoc($result)) {
+   $encode[] = $row;
+}
+
+echo json_encode($encode);
+
+if(mysqli_query($connection,$sql_query))
+{
+  $httpStatusCode = 200;
+  $httpStatusMsg  = 'OK';
+  header('Status: '.$httpStatusCode.' '.$httpStatusMsg);
+  //echo "successful\n";
+}
+else{
+  $httpStatusCode = 304;
+  $httpStatusMsg  = 'Not Modified';
+  header('Status: '.$httpStatusCode.' '.$httpStatusMsg);
+  //echo "error\n";
+}
+?>
