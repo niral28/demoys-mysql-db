778e8f87aee71709afd15776a325ba18f161cc02
Niral Shah <niralshah928@gmail.com>
2017-08-08 00:37:34 -0700

    testing changes to HTTP get request

--- web/data.php
+++ web/data.php
@@ -14,7 +14,7 @@ if($result)
   header('Status: '.$httpStatusCode.' '.$httpStatusMsg);
 
   $encode = array();
-  if(mysqli_num_rows($result)){
+  if(mysqli_num_rows($result)>0){
     while($row = mysqli_fetch_assoc($result)) {
        $encode[] = $row;
     }
