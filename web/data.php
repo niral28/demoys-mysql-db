79e24967033be33636902cefa37985280e120888
Niral Shah <niralshah928@gmail.com>
2017-08-08 01:11:55 -0700

    added where clause back

--- web/data.php
+++ web/data.php
@@ -1,7 +1,7 @@
 <?php
 
 require "init.php";
-$u_username  = $_POST["username"];
+$u_username  = $_GET["username"];
 
 
 $sql_query="SELECT * from user_data where username like '" . $u_username . "'";
