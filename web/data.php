8fdf9b1aa342ba00f1181837d1e06d5ddf14c403
Niral Shah <niralshah928@gmail.com>
2017-08-08 00:13:01 -0700

    testing changes to HTTP get request

--- web/data.php
+++ web/data.php
@@ -5,21 +5,23 @@ $u_username  = $_GET["username"];
 
 
 $sql_query="SELECT * from user_data where username like '$u_username'";
+$result = mysqli_query($connection,$sql_query)
 
-$encode = array();
-
-while($row = mysqli_fetch_assoc($sql_query)) {
-   $encode[] = $row;
-}
-
-echo json_encode($encode);
-
-if(mysqli_query($connection,$sql_query))
+if($result)
 {
   $httpStatusCode = 200;
   $httpStatusMsg  = 'OK';
   header('Status: '.$httpStatusCode.' '.$httpStatusMsg);
-  //echo "successful\n";
+
+  $encode = array();
+  if(mysqli_num_rows($result)){
+    while($row = mysqli_fetch_assoc($result)) {
+       $encode[] = $row;
+    }
+    echo json_encode($encode);
+ }else {
+   echo "[]"
+ }
 }
 else{
   $httpStatusCode = 304;
